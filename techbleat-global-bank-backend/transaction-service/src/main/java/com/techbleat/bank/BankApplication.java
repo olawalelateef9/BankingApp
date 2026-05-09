22b19220ed5832d47b9a28b5c84819dfc5f4e4b1
olawale lateef <olawalelateef9@gmail.com>
2026-05-08 22:18:30 -1200

    push_17

--- techbleat-global-bank-backend/transaction-service/src/main/java/com/techbleat/bank/BankApplication.java
+++ techbleat-global-bank-backend/transaction-service/src/main/java/com/techbleat/bank/BankApplication.java
@@ -2,8 +2,10 @@ package com.techbleat.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling; // Added for 5-min session cleanup
 
 @SpringBootApplication
+@EnableScheduling // Added to enable the @Scheduled task in TransactionService
 public class BankApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankApplication.class, args);
